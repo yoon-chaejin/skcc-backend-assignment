f932ba2ec830f2bb9aa544a49ed6cd66fea0f548
jaeyoung choi <kevin3st@naver.com>
2023-11-15 07:07:14 +0000

    chore : initial commit

---
+++ src/main/java/com/example/skccbackendassignment/SkccBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.skccbackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkccBackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkccBackendAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skccbackendassignment/SkccBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skccbackendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkccBackendAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
